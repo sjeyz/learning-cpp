5aab9e6cea616038ec9b185929fd2fb44cf6c9a0
Sawera  jamshaid <sawerajamshaid0@gmail.com>
2024-12-17 06:31:40 +0500

    con

--- main.cpp
+++ main.cpp
@@ -51,3 +51,22 @@ int main() {
 
 }
 
+#include <istream>
+ using namespace std;
+
+int main(){
+  int n;
+       cout<<"enter the postive integar :";
+      cin >> number;
+  if(n >0){
+    if(number % 5 == 0){
+      cout<< number<<"divisible by 5"<<endl;
+    }
+    else{
+      cout<<number<<"not divisible by 5"<<endl;
+    }
+    else{
+    cout <<"please enter a positive number integer"<<endl;
+    }
+  }                                                                                                          
+}
